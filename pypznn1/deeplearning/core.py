a7bfd472dae8b0ebebbd227c6898cc09445fbbf1
ohmin <ohmin@example.com>
2021-10-23 10:45:47 +0900

    Implementing deep learning framework/step06:back propagation by hand

--- pypznn1/deeplearning/core.py
+++ pypznn1/deeplearning/core.py
@@ -3,22 +3,39 @@ import numpy as np
 class Variable:
     def __init__(self, data):
         self.data = data
+        self.grad = None
 
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
         output = Variable(y)
+        self.input = input
         return output
 
     def forward(self, x):
         raise NotImplementedError()
 
+    def backward(self, gy):
+        raise NotImplementedError()
+
 class Square(Function):
     def forward(self, x):
-        return x ** 2
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = 2 * x * gy
+        return gx
 
 class Exp(Function):
     def forward(self, x):
-        return np.exp(x)
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.input.data
+        gx = np.exp(x) * gy
+        return gx
 
---
+++ pypznn1/deeplearning/step06.py
@@ -0,0 +1,18 @@
+import numpy as np
+from .core import Variable, Square, Exp
+
+A = Square()
+B = Exp()
+C = Square()
+
+x = Variable(np.array(0.5))
+a = A(x)
+b = B(a)
+y = C(b)
+
+y.grad = np.array(1.0)
+b.grad = C.backward(y.grad)
+a.grad = B.backward(b.grad)
+x.grad = A.backward(a.grad)
+print(x.grad)
+
