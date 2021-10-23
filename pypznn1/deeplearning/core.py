53158f219876fce5d6363382eae50af795bf7785
ohmin <ohmin@example.com>
2021-10-23 11:41:45 +0900

    Implementing deep learning framework/step09:three improvements

--- pypznn1/deeplearning/core.py
+++ pypznn1/deeplearning/core.py
@@ -2,6 +2,10 @@ import numpy as np
 
 class Variable:
     def __init__(self, data):
+        if data is not None:
+            if not isinstance(data, np.ndarray):
+                raise TypeError(f"{type(data)} is not supported.")
+
         self.data = data
         self.grad = None
         self.creator = None
@@ -10,6 +14,9 @@ class Variable:
         self.creator = func
 
     def backward(self):
+        if self.grad is None:
+            self.grad = np.ones_like(self.data)
+
         funcs = [self.creator]
         while funcs:
             f = funcs.pop()
@@ -18,11 +25,16 @@ class Variable:
             if x.creator is not None:
                 funcs.append(x.creator)
 
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
+
 class Function:
     def __call__(self, input):
         x = input.data
         y = self.forward(x)
-        output = Variable(y)
+        output = Variable(as_array(y))
         output.set_creator(self)
         self.input = input
         self.output = output
@@ -53,4 +65,12 @@ class Exp(Function):
         x = self.input.data
         gx = np.exp(x) * gy
         return gx
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
 
