8efc4059d335a12d37d8ad27396318788aa9fca7
ohmin839 <ohminato.oshima@gmail.com>
2021-10-25 11:20:34 +0900

    Implementing deep learning framework/step27:differentiation of Taylor expansion

--- pypznn1/deeplearning/functions.py
+++ pypznn1/deeplearning/functions.py
@@ -1,30 +1,5 @@
-class Square(Function):
-    def forward(self, x):
-        y = x ** 2
-        return y
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = 2 * x * gy
-        return gx
-
-class Exp(Function):
-    def forward(self, x):
-        y = np.exp(x)
-        return y
-
-    def backward(self, gy):
-        x = self.inputs[0].data
-        gx = np.exp(x) * gy
-        return gx
-
-def square(x):
-    f = Square()
-    return f(x)
-
-def exp(x):
-    f = Exp()
-    return f(x)
+import numpy as np
+from pypznn1.deeplearning import Function
 
 class Square(Function):
     def forward(self, x):
@@ -52,5 +27,47 @@ def square(x):
 
 def exp(x):
     f = Exp()
+    return f(x)
+
+class Square(Function):
+    def forward(self, x):
+        y = x ** 2
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = 2 * x * gy
+        return gx
+
+class Exp(Function):
+    def forward(self, x):
+        y = np.exp(x)
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = np.exp(x) * gy
+        return gx
+
+class Sin(Function):
+    def forward(self, x):
+        y = np.sin(x)
+        return y
+
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = gy * np.cos(x)
+        return gx
+
+def square(x):
+    f = Square()
+    return f(x)
+
+def exp(x):
+    f = Exp()
+    return f(x)
+
+def sin(x):
+    f = Sin()
     return f(x)
 
