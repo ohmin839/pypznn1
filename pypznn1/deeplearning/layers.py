506c7d28fe0e12a4ec8de76c840f7c65b0582f9e
ohmin <ohmin@example.com>
2021-11-28 00:24:34 +0900

    Added BatchNorm layer

--- pypznn1/deeplearning/layers.py
+++ pypznn1/deeplearning/layers.py
@@ -236,3 +236,28 @@ class LSTM(Layer):
         self.h, self.c = h_new, c_new
         return h_new
 
+class BatchNorm(Layer):
+    def __init__(self):
+        super().__init__()
+        self.avg_mean = Parameter(None, name='avg_mean')
+        self.avg_var = Parameter(None, name='avg_var')
+        self.gamma = Parameter(None, name='gamma')
+        self.beta = Parameter(None, name='beta')
+
+    def _init_params(self, x):
+        xp = cuda.get_array_module(x)
+        D = x.shape[1]
+        if self.avg_mean.data is None:
+            self.avg_mean.data = xp.zeros(D, dtype=x.dtype)
+        if self.avg_var.data is None:
+            self.avg_var.data = xp.ones(D, dtype=x.dtype)
+        if self.gamma.data is None:
+            self.gamma.data = xp.ones(D, dtype=x.dtype)
+        if self.beta.data is None:
+            self.beta.data = xp.zeros(D, dtype=x.dtype)
+
+    def __call__(self, x):
+        if self.avg_mean.data is None:
+            self._init_params(x)
+        return F.batch_norm(x, self.gamma, self.beta,
+                            self.avg_mean.data, self.avg_var.data)
